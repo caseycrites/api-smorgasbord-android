c7ad31b49c1c0474dd350108994f90471b6694b9
Casey W Crites <crites.casey@gmail.com>
2011-09-26 15:37:27 -0700

    Added a little documentation about SGListener

--- src/com/simplegeo/android/util/SGListener.java
+++ src/com/simplegeo/android/util/SGListener.java
@@ -2,10 +2,26 @@ package com.simplegeo.android.util;
 
 import android.os.Bundle;
 
+/**
+ * Smorgasbord specific Listener for asynchronous methods.
+ * 
+ * @author caseycrites
+ *
+ */
 public interface SGListener {
 	
+	/**
+	 * Callback method for successful operations.
+	 * 
+	 * @param values
+	 */
 	public void onSuccess(Bundle values);
 	
+	/**
+	 * Callback method for erred operations.
+	 * 
+	 * @param values
+	 */
 	public void onError(Bundle values);
 
 }
